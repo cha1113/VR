1d61011d7a724f51ab0d78dc1dcbd609fcd801d6
cha1113 <160296102+cha1113@users.noreply.github.com>
2024-04-18 10:33:20 +0800

    home

---
+++ home/data.js
@@ -0,0 +1,45 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-",
+      "name": "天壇",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 1024,
+      "initialViewParameters": {
+        "yaw": 0.07588158822242974,
+        "pitch": -0.29591515863381446,
+        "fov": 1.2828048894984105
+      },
+      "linkHotspots": [],
+      "infoHotspots": [
+        {
+          "yaw": -0.8159286405996227,
+          "pitch": -0.42501146379667887,
+          "title": "關渡宮",
+          "text": "前身天妃廟，草創於康熙51年(1712)，主祀天上聖母媽祖，成為北台灣最早的媽祖廟。<div>當時流傳:「南有北港媽，北有干豆媽」</div>"
+        }
+      ]
+    }
+  ],
+  "name": "home",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": true,
+    "fullscreenButton": false,
+    "viewControlButtons": false
+  }
+};
