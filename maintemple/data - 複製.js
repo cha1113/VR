8268b958ae9415513f7a5abe2d8ff30e9a36d749
cha1113 <160296102+cha1113@users.noreply.github.com>
2024-04-18 10:58:05 +0800

    maintemple

---
+++ maintemple/data - 複製.js
@@ -0,0 +1,523 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-",
+      "name": "天壇",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 1024,
+      "initialViewParameters": {
+        "yaw": 0.04808560184065058,
+        "pitch": -0.2264392296991602,
+        "fov": 1.2599180821480807
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.10272080325628608,
+          "pitch": 0.004934540842050694,
+          "rotation": 0,
+          "target": "2-"
+        },
+        {
+          "yaw": 2.5365762479616594,
+          "pitch": 0.058680889939875414,
+          "rotation": 0,
+          "target": "1-"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 0.44625941873043473,
+          "pitch": -0.32957866176707107,
+          "title": "山川殿",
+          "text": "進出關渡宮的正式門戶。<div>「入龍喉‚出虎口」左青龍‚右白虎</div><div>遵守左進右出的原則。</div><div>(左右以神明尊身的方向為主哦！)</div>"
+        },
+        {
+          "yaw": -2.3059388897055655,
+          "pitch": -0.4189428088739131,
+          "title": "天壇",
+          "text": "作為酬神演戲、春節花燈造景之用。"
+        }
+      ]
+    },
+    {
+      "id": "1-",
+      "name": "山門",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "yaw": 0.10728042401957083,
+        "pitch": -0.5039554225496872,
+        "fov": 1.2599180821480807
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.04011253364844336,
+          "pitch": -0.07790807384170151,
+          "rotation": 0,
+          "target": "0-"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -0.6041940358675149,
+          "pitch": -0.6518450152225554,
+          "title": "山門",
+          "text": "<div>又稱為「牌樓」或「牌坊」，位於進出關渡宮的階梯口。</div><div>正面門楣的匾額題有「關渡宮」三字，背面額則題「開山啟教」四字，以紀念三百年前祖師開山，宣揚宗教、啟人教化。</div>"
+        }
+      ]
+    },
+    {
+      "id": "2-",
+      "name": "媽祖殿",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "yaw": 0.007698948012549778,
+        "pitch": -0.02484992455643109,
+        "fov": 1.2599180821480807
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.8962686205661576,
+          "pitch": 0.2897996059459125,
+          "rotation": 1.5707963267948966,
+          "target": "3-"
+        },
+        {
+          "yaw": -0.9206414549321593,
+          "pitch": 0.26813142898420494,
+          "rotation": 4.71238898038469,
+          "target": "4-"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -0.011932922566051829,
+          "pitch": 0.13245046385497972,
+          "title": "天上聖母",
+          "text": "Text"
+        }
+      ]
+    },
+    {
+      "id": "3-",
+      "name": "觀音佛祖殿",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "yaw": 0.03383801611009574,
+        "pitch": 0.007632783023566958,
+        "fov": 1.2599180821480807
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.9080540541347482,
+          "pitch": 0.27394734851593583,
+          "rotation": 4.71238898038469,
+          "target": "2-"
+        },
+        {
+          "yaw": 1.000367338428779,
+          "pitch": 0.25558369516925517,
+          "rotation": 5.497787143782138,
+          "target": "9-"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 0.2817011324860008,
+          "pitch": -0.12601048843256102,
+          "title": "觀音佛祖",
+          "text": "Text"
+        }
+      ]
+    },
+    {
+      "id": "4-",
+      "name": "文昌帝君殿",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "yaw": -3.09678990618686,
+        "pitch": -0.04772956583891386,
+        "fov": 1.2599180821480807
+      },
+      "linkHotspots": [
+        {
+          "yaw": -2.1679373717585815,
+          "pitch": 0.26264882993310756,
+          "rotation": 1.5707963267948966,
+          "target": "2-"
+        },
+        {
+          "yaw": 2.270011093053604,
+          "pitch": 0.2831331946297144,
+          "rotation": 11.780972450961727,
+          "target": "5-"
+        },
+        {
+          "yaw": 2.2374301521629043,
+          "pitch": -0.05521637339204766,
+          "rotation": 7.0685834705770345,
+          "target": "6-"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -2.894871737326209,
+          "pitch": -0.07649150061429921,
+          "title": "文昌帝君",
+          "text": "Text"
+        }
+      ]
+    },
+    {
+      "id": "5-",
+      "name": "延平郡王三將軍殿",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "yaw": -0.30988498963981215,
+        "pitch": -0.012721305039278263,
+        "fov": 1.2599180821480807
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.6303653767031676,
+          "pitch": 0.2784899818416111,
+          "rotation": 1.5707963267948966,
+          "target": "4-"
+        },
+        {
+          "yaw": 0.6275768874825758,
+          "pitch": 0.06029982090042196,
+          "rotation": 5.497787143782138,
+          "target": "6-"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 0.3800206671223183,
+          "pitch": 0.37746307064665174,
+          "title": "延平郡王三將軍廟",
+          "text": "Text"
+        },
+        {
+          "yaw": -0.3894512065225193,
+          "pitch": -0.20127823261239364,
+          "title": "延平郡王",
+          "text": "Text"
+        }
+      ]
+    },
+    {
+      "id": "6-",
+      "name": "財神爺",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "yaw": 0.024933275028487856,
+        "pitch": -0.02035408806284167,
+        "fov": 1.2599180821480807
+      },
+      "linkHotspots": [
+        {
+          "yaw": 3.1093073258819235,
+          "pitch": 0.42725250946675786,
+          "rotation": 5.497787143782138,
+          "target": "7-"
+        },
+        {
+          "yaw": -0.907352249811133,
+          "pitch": 0.2811893227509046,
+          "rotation": 5.497787143782138,
+          "target": "9-"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -0.027593139831347813,
+          "pitch": 0.05054796918899207,
+          "title": "天官賜福",
+          "text": "Text"
+        }
+      ]
+    },
+    {
+      "id": "7-",
+      "name": "財神洞",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "yaw": -0.18878051093000892,
+        "pitch": -0.0254326179254889,
+        "fov": 1.2599180821480807
+      },
+      "linkHotspots": [
+        {
+          "yaw": -1.672407356821381,
+          "pitch": 0.20449809140901998,
+          "rotation": 0.7853981633974483,
+          "target": "6-"
+        },
+        {
+          "yaw": 1.5387479876458023,
+          "pitch": 0.3578342558064538,
+          "rotation": 5.497787143782138,
+          "target": "8-"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -0.7690793839053942,
+          "pitch": -0.349736765030368,
+          "title": "財神洞",
+          "text": "Text"
+        }
+      ]
+    },
+    {
+      "id": "8-",
+      "name": "福德正殿",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "yaw": 0.1495996501709449,
+        "pitch": -0.0432524371335532,
+        "fov": 1.2599180821480807
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.4670956243859372,
+          "pitch": 0.23545046784767898,
+          "rotation": 0.7853981633974483,
+          "target": "7-"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 0.3633331821156336,
+          "pitch": 0.07519373364710624,
+          "title": "福德正殿",
+          "text": "Text"
+        }
+      ]
+    },
+    {
+      "id": "9-",
+      "name": "鐘樓",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.25058760340318287,
+          "pitch": 0.13709765644247796,
+          "rotation": 5.497787143782138,
+          "target": "6-"
+        },
+        {
+          "yaw": -2.9048528974128818,
+          "pitch": 0.2217051796609475,
+          "rotation": 0.7853981633974483,
+          "target": "3-"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 1.6307067830748974,
+          "pitch": -0.014291428349611834,
+          "title": "功德堂",
+          "text": "Text"
+        }
+      ]
+    }
+  ],
+  "name": "maintemple",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": false,
+    "fullscreenButton": false,
+    "viewControlButtons": false
+  }
+};
