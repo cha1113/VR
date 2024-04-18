cef071c62baa3ca150b82eb8d572c9357e1fdcc0
cha1113 <160296102+cha1113@users.noreply.github.com>
2024-04-18 11:55:33 +0800

    building

---
+++ building/data.js
@@ -0,0 +1,480 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-",
+      "name": "鐘樓",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.19759807334382984,
+          "pitch": 0.05829185413882598,
+          "rotation": 0,
+          "target": "4-"
+        },
+        {
+          "yaw": 0.6686804718389912,
+          "pitch": -0.05943098055819007,
+          "rotation": 5.497787143782138,
+          "target": "1-"
+        },
+        {
+          "yaw": -0.2999625959370995,
+          "pitch": 0.08701790588188629,
+          "rotation": 0,
+          "target": "9-"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "1-",
+      "name": "樓梯",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 896,
+      "initialViewParameters": {
+        "yaw": -0.6536079953897165,
+        "pitch": -0.22003947693283976,
+        "fov": 1.2828048894984105
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.635415418636363,
+          "pitch": -0.24061793874860626,
+          "rotation": 11.780972450961727,
+          "target": "2-"
+        },
+        {
+          "yaw": -3.079673035519395,
+          "pitch": 0.6587978541588377,
+          "rotation": 0,
+          "target": "0-"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "2-",
+      "name": "靈山公園",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 896,
+      "initialViewParameters": {
+        "yaw": 0.11932287265834418,
+        "pitch": 0.017705259945895335,
+        "fov": 1.2828048894984105
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.3211518898515564,
+          "pitch": 0.662663565887911,
+          "rotation": 3.9269908169872414,
+          "target": "1-"
+        },
+        {
+          "yaw": 1.3413327261591608,
+          "pitch": -0.03296561170202317,
+          "rotation": 0.7853981633974483,
+          "target": "3-"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "3-",
+      "name": "靈山公園頂",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 896,
+      "initialViewParameters": {
+        "yaw": -2.5662169084910964,
+        "pitch": -0.035117878234949274,
+        "fov": 1.2828048894984105
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.12928631754438946,
+          "pitch": 0.14139029058618569,
+          "rotation": 2.356194490192345,
+          "target": "2-"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "4-",
+      "name": "靈山古佛洞入口",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 896,
+      "initialViewParameters": {
+        "yaw": -0.01959043037951247,
+        "pitch": -0.2579773177833182,
+        "fov": 1.2828048894984105
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.002943388964066429,
+          "pitch": 0.2737250971581151,
+          "rotation": 0,
+          "target": "5-"
+        },
+        {
+          "yaw": 3.045709161866336,
+          "pitch": 0.4962568120180322,
+          "rotation": 0,
+          "target": "0-"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 0.8069658397116832,
+          "pitch": -0.1339128720281515,
+          "title": "古佛洞",
+          "text": "是靈山公園之下"
+        }
+      ]
+    },
+    {
+      "id": "5-",
+      "name": "靈山古佛洞前段",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 896,
+      "initialViewParameters": {
+        "yaw": -2.923742767753918,
+        "pitch": 0.06398933301498388,
+        "fov": 1.2828048894984105
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.024497915974526663,
+          "pitch": 0.45719156185961474,
+          "rotation": 0,
+          "target": "4-"
+        },
+        {
+          "yaw": -3.126211052291051,
+          "pitch": 0.2648966971199407,
+          "rotation": 0,
+          "target": "6-"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "6-",
+      "name": "靈山古佛洞後段",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 896,
+      "initialViewParameters": {
+        "yaw": 0.19768525201160614,
+        "pitch": -0.05817135597074241,
+        "fov": 1.2828048894984105
+      },
+      "linkHotspots": [
+        {
+          "yaw": -2.962586018659694,
+          "pitch": 0.4898792720762586,
+          "rotation": 0,
+          "target": "5-"
+        },
+        {
+          "yaw": -0.008380965475257796,
+          "pitch": 0.38506585880415223,
+          "rotation": 0,
+          "target": "7-"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "7-",
+      "name": "千手觀音殿",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "yaw": -0.17868406654216074,
+        "pitch": -0.13404703767173132,
+        "fov": 1.2828048894984105
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.1728687633933106,
+          "pitch": 0.02943761695782854,
+          "rotation": 0.7853981633974483,
+          "target": "6-"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -1.0090070876178014,
+          "pitch": 0.1135278440449543,
+          "title": "千手千眼觀音",
+          "text": "Text"
+        }
+      ]
+    },
+    {
+      "id": "8--2",
+      "name": "凌霄寶殿-2",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "yaw": 0.09082835903236841,
+        "pitch": -0.3338529994842965,
+        "fov": 1.2828048894984105
+      },
+      "linkHotspots": [
+        {
+          "yaw": 1.5333621227678282,
+          "pitch": 0.1288036250428135,
+          "rotation": 3.141592653589793,
+          "target": "9-"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -0.0581706604233716,
+          "pitch": -0.6053382560904126,
+          "title": "三官大帝",
+          "text": "Text"
+        }
+      ]
+    },
+    {
+      "id": "9-",
+      "name": "干豆媽文化館前段",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 2.191702803050095,
+          "pitch": 0.23012915354326147,
+          "rotation": 0.7853981633974483,
+          "target": "10-"
+        },
+        {
+          "yaw": -0.5579374051438215,
+          "pitch": 0.006003332432356956,
+          "rotation": 0,
+          "target": "8--2"
+        },
+        {
+          "yaw": -0.5675471813064981,
+          "pitch": 0.19113242566259103,
+          "rotation": 2.356194490192345,
+          "target": "0-"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "10-",
+      "name": "干豆媽文化館後段",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 2.017804482832495,
+          "pitch": 0.11951508029357427,
+          "rotation": 4.71238898038469,
+          "target": "9-"
+        }
+      ],
+      "infoHotspots": []
+    }
+  ],
+  "name": "靈霄寶殿",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": true,
+    "fullscreenButton": false,
+    "viewControlButtons": false
+  }
+};
